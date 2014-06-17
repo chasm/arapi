1cc02d41c735e9d3eda13365956ad3450000da1e
Charles F. Munat <charles.munat@gmail.com>
2014-06-17 16:21:43 +0800

    Added UUID generator and controller/routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Arapi::Application.routes.draw do
-  resources :jobs
-
-  resources :duties
-
-  resources :comments
-
-  resources :tags
-
-  resources :articles
-
   # root 'welcome#index'
+
+  get 'uuids'      => 'uuids#create'
+  get 'uuids/:num' => 'uuids#create'
 end
