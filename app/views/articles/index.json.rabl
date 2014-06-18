429a248a470d2b526f313477e79457c32a72a52c
Charles F. Munat <charles.munat@gmail.com>
2014-06-18 11:26:00 +0800

    Added metadata to articles and fixed root page.

--- app/views/articles/index.json.rabl
+++ app/views/articles/index.json.rabl
@@ -1,3 +1,31 @@
-collection @articles => :articles
+object false
 
-attributes :id, :title, :body
+child @articles do
+  attributes :id, :title, :body
+
+  node :tags do |article|
+    article.tags.map {|t| t.name }
+  end
+
+  node :links do |article|
+    {
+      user: article.user.id,
+      comments: article.comments.map {|c| c.id }
+    }
+  end
+
+  node :href do |article|
+    article_url(article)
+  end
+end
+
+node :links do
+  {
+    users: users_url + "/{users.id}",
+    comments: comments_url + "/{comments.id}"
+  }
+end
+
+node :meta do
+  { "client-ids" => true }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,54 +1,56 @@
 Arapi::Application.routes.draw do
-  root 'api#index'
+  scope defaults: { format: :json } do
+    root 'api#index'
 
-  get 'uuids'      => 'uuids#create', as: 'uuid'
-  get 'uuids/:num' => 'uuids#create', as: 'uuids'
+    get 'uuids'      => 'uuids#create', as: 'uuid'
+    get 'uuids/:num' => 'uuids#create', as: 'uuids'
 
-  scope :articles, defaults: { format: :json } do
-    get    ''     => 'articles#index', as: 'articles'
-    get    ':ids' => 'articles#index', as: 'article'
-    put    ':id'  => 'articles#create_or_replace'
-    patch  ':id'  => 'articles#update'
-    delete ':id'  => 'articles#destroy'
-  end
+    scope :articles do
+      get    ''     => 'articles#index', as: 'articles'
+      get    ':ids' => 'articles#index', as: 'article'
+      put    ':id'  => 'articles#create_or_replace'
+      patch  ':id'  => 'articles#update'
+      delete ':id'  => 'articles#destroy'
+    end
 
-  scope :comments, defaults: { format: :json } do
-    get    ''     => 'comments#index', as: 'comments'
-    get    ':ids' => 'comments#index', as: 'comment'
-    put    ':id'  => 'comments#create_or_replace'
-    patch  ':id'  => 'comments#update'
-    delete ':id'  => 'comments#destroy'
-  end
+    scope :comments do
+      get    ''     => 'comments#index', as: 'comments'
+      get    ':ids' => 'comments#index', as: 'comment'
+      put    ':id'  => 'comments#create_or_replace'
+      patch  ':id'  => 'comments#update'
+      delete ':id'  => 'comments#destroy'
+    end
 
-  scope :duties, defaults: { format: :json } do
-    get    ''     => 'duties#index', as: 'duties'
-    get    ':ids' => 'duties#index', as: 'duty'
-    put    ':id'  => 'duties#create_or_replace'
-    patch  ':id'  => 'duties#update'
-    delete ':id'  => 'duties#destroy'
-  end
+    scope :duties do
+      get    ''     => 'duties#index', as: 'duties'
+      get    ':ids' => 'duties#index', as: 'duty'
+      put    ':id'  => 'duties#create_or_replace'
+      patch  ':id'  => 'duties#update'
+      delete ':id'  => 'duties#destroy'
+    end
 
-  scope :jobs, defaults: { format: :json } do
-    get    ''     => 'jobs#index', as: 'jobs'
-    get    ':ids' => 'jobs#index', as: 'job'
-    put    ':id'  => 'jobs#create_or_replace'
-    patch  ':id'  => 'jobs#update'
-    delete ':id'  => 'jobs#destroy'
-  end
+    scope :jobs do
+      get    ''     => 'jobs#index', as: 'jobs'
+      get    ':ids' => 'jobs#index', as: 'job'
+      put    ':id'  => 'jobs#create_or_replace'
+      patch  ':id'  => 'jobs#update'
+      delete ':id'  => 'jobs#destroy'
+    end
 
-  scope :tags, defaults: { format: :json } do
-    get    ''     => 'tags#index', as: 'tags'
-    get    ':ids' => 'tags#index', as: 'tag'
-    put    ':id'  => 'tags#create_or_replace'
-    patch  ':id'  => 'tags#update'
-    delete ':id'  => 'tags#destroy'
-  end
+    scope :tags do
+      get    ''     => 'tags#index', as: 'tags'
+      get    ':ids' => 'tags#index', as: 'tag'
+      put    ':id'  => 'tags#create_or_replace'
+      patch  ':id'  => 'tags#update'
+      delete ':id'  => 'tags#destroy'
+    end
 
-  scope :users, defaults: { format: :json } do
-    get    ''     => 'users#index', as: 'users'
-    get    ':ids' => 'users#index', as: 'user'
-    put    ':id'  => 'users#create_or_replace'
-    patch  ':id'  => 'users#update'
-    delete ':id'  => 'users#destroy'
+    scope :users do
+      get    ''     => 'users#index', as: 'users'
+      get    ':ids' => 'users#index', as: 'user'
+      put    ':id'  => 'users#create_or_replace'
+      patch  ':id'  => 'users#update'
+      delete ':id'  => 'users#destroy'
+    end
   end
 end
